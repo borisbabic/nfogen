681b0f587d550ffe364ffa7170714bd5a8cadcce
Boris Babic <boris.ivan.babic@gmail.com>
2015-07-05 18:55:46 +0200

    initial commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+Usage:
+
+python __init__.py $directory
+nfogen $directory
+
+Walks through subdirectories in $directory and creates a tvshow.nfo with the url to tvdb.
+The user is prompted for which show to use
---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='nfogen',
+    version='0.0.1',
+    author='Boris Babic',
+    author_email='boris.ivan.babic@gmail.com',
+    packages=['nfogen'],
+    include_package_data=True,
+    url='template url',
+    entry_points={
+            'console_scripts': [
+                'nfogen = nmanager:main',
+            ],
+        },
+    install_requires=[
+        "tvdb_api",
+        ],
+    description='Generate tvshow.nfo files for tv shows',
+    long_description=open('README.txt').read(),
+)
